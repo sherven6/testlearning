0570a544265700e209bdd2452f0e4d99809347cc
zsherven6 <zsherven@outlook.com>
2019-04-15 21:10:55 +0800

    增加了base_driver的获取toast参数

--- base/base_driver.py
+++ base/base_driver.py
@@ -4,11 +4,11 @@ desired_caps = {
     "platformName": "android",
     "platVersion": 8,
     "deviceName": "sherven",
+    "automationName": "UIautomator2",
     "appPackage": "com.android.settings",
     "appActivity": ".Settings"
 }
 
 
 def initDriver():
-    dr = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
-    return dr
+    return webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
